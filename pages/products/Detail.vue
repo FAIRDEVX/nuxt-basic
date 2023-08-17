d69399279cd6ed9a53de91e22723145c36d906c7
FAIRDEVX <115607341+FAIRDEVX@users.noreply.github.com>
2023-08-17 22:14:31 +0700

    update
    create pages

---
+++ pages/products/Detail.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>รายละเอียดของสินค้า</h1>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/products/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>สินค้าทั้งหมด</h1>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
